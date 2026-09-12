b2851bfce5f23c50b2f97e395c83be1136020ab9
Gabefry <167709107+Gabefry@users.noreply.github.com>
2026-09-12 14:42:46 -0500

    Feeder Classs

--- Peashooter/src/main/java/frc/robot/Robot.java
+++ Peashooter/src/main/java/frc/robot/Robot.java
@@ -4,10 +4,12 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Subsystems.Drive;
+import frc.robot.Subsystems.Feeder;
 
 public class Robot extends TimedRobot {
 
   Drive drive = new Drive();
+  Feeder feeder = new Feeder();
   CommandXboxController controller = new CommandXboxController(0);
 
   public Robot() {
@@ -15,9 +17,15 @@ public class Robot extends TimedRobot {
     drive.setDefaultCommand(drive.drive(
         () -> controller.getLeftX(),
         () -> controller.getLeftY(),
-        () -> controller.getRightX()));
+        () -> controller.getRightX()
+      )
+    );
 
-        controller.a().onTrue(drive.toggleFieldRelative());
+    controller.rightBumper().whileTrue(feeder.load(true)); //load
+    controller.leftBumper().whileTrue(feeder.load(false)); //unload
+    controller.a().onTrue(drive.toggleFieldRelative());
+
+
 }
 
  
--- Peashooter/src/main/java/frc/robot/Subsystems/Feeder.java
+++ Peashooter/src/main/java/frc/robot/Subsystems/Feeder.java
@@ -1,7 +1,48 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feeder extends SubsystemBase{
+
+    /*
+     * 
+     * 
+     * 
+     */
     
-}
+    SparkMax spindexterMotor = new SparkMax(10, MotorType.kBrushless);
+    SparkMaxConfig spindexterConfig = new SparkMaxConfig();
+
+    SparkFlex towerMotor = new SparkFlex(11, MotorType.kBrushless);
+    SparkFlexConfig towerConfig = new SparkFlexConfig();
+    
+    public Feeder(){
+        spindexterConfig.idleMode(SparkMaxConfig.IdleMode.kBrake);
+        spindexterMotor.configure(towerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        towerConfig.idleMode(IdleMode.kBrake).follow(spindexterMotor);  
+        towerMotor.configure(towerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        
+    }
+
+    private void runFeeder(boolean forward){
+        spindexterMotor.set(forward ? -1 : 1);
+    }
+
+    private void stopFeeder() {
+        spindexterMotor.stopMotor();
+    }
+
+    public Command load(boolean forward){
+        return this.runEnd(() -> runFeeder(forward), () -> stopFeeder());
+    }
+}   
