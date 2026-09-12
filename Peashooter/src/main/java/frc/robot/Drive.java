2c5c3da6f0ba016eff270e65338e49eed9ed81d1
RCar-maker <creeperjay77@gmail.com>
2026-09-12 12:44:21 -0500

    hi im kadiri

---
+++ Peashooter/src/main/java/frc/robot/Drive.java
@@ -0,0 +1,43 @@
+package frc.robot;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+
+public class Drive extends SubsystemBase {
+    private SwerveDrive swerveDrive;
+    private boolean fieldR = true;
+
+     Drive() {
+        File swerveConfigDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
+        try {
+            swerveDrive = new SwerveParser(swerveConfigDirectory).createSwerveDrive(40,
+             new Pose2d(new Translation2d(2,2), new Rotation2d()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Command drive(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier rotation) {
+    return this.run(() -> {
+        swerveDrive.drive(new Translation2d(translationX.getAsDouble(), translationY.getAsDouble()),
+            rotation.getAsDouble(),
+            fieldR,
+            false);
+    });
+
+    
+}
+    public Command toggleFieldRelative() {
+    return this.runOnce(() -> fieldR = !fieldR);
+}
+}
---
+++ Peashooter/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,58 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+public class Robot extends TimedRobot {
+
+  Drive drive = new Drive();
+  CommandXboxController controller = new CommandXboxController(0);
+
+  public Robot() {
+
+    drive.setDefaultCommand(drive.drive(
+        () -> controller.getLeftX(),
+        () -> controller.getLeftY(),
+        () -> controller.getRightX()));
+
+        controller.a().onTrue(drive.toggleFieldRelative());
+}
+
+ 
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
